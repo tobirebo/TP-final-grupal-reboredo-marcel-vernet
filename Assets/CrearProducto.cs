8d7d7dced067537c0fa25dac42d69960d6162852
tobirebo <48115989@est.ort.edu.ar>
2024-06-27 12:05:55 -0300

    codigo y objetos

---
+++ Assets/CrearProducto.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrearProducto : MonoBehaviour
+{
+    public GameObject[] objetos;
+    private int objetosIndex;
+    void Start()
+    {
+        objetosIndex = Random.Range(1, 4);
+        Debug.Log("Índice aleatorio: " + objetosIndex);
+
+        foreach (GameObject g in objetos)
+        {
+            g.SetActive(false);
+        }
+
+        objetos[objetosIndex].SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
