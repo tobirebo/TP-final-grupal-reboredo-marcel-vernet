4c85690ea03a7c38c75bd7cd974cf9a41133845e
tobirebo <48115989@est.ort.edu.ar>
2024-07-01 08:25:55 -0300

    codigo nuevo

---
+++ Assets/texoscript.cs
@@ -0,0 +1,24 @@
+﻿  using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class texoscript : MonoBehaviour
+{
+    public Text operacion;
+    public int valortotal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        valortotal = Random.Range(1, 21);
+        Debug.Log("Valor total: " + valortotal);
+
+        operacion.text = "+  ?  =  " + valortotal.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
